da56c363a2dca37cee1213ecd2e59c709fb6bc79
jeremyIsGREat <131487568+jeremyIsGREat@users.noreply.github.com>
2023-06-19 01:23:13 -0400

    git commit -m "added functionality
    Co-authored-by: Tesfa <tesfaget15@gmail.com>"

---
+++ practice.js
@@ -0,0 +1,11 @@
+function minSubArrayLen(nums, val) {
+    let sum=0
+    let rightPointer=0
+    let left=0;
+        
+            
+    if(rightPointer===1)return rightPointer
+    
+    
+    
+}
